425b29eececee42279dd0e370c0cda293608c371
Luka Stromajer <lukalel.kalulel@gmail.com>
2025-12-09 21:23:04 +0100

    Add initial event listeners to the buttons, not done yet

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Result
-let result = document.querySelector('.result');
+let result = document.querySelector(".result");
 
 // Function to update the result text
 function updateResult(text) {
     result.textContent = text;
 }
+
+
+
+
+
+// First number
+let num1;
+let buttons = Array.from(document.querySelectorAll(".num"));
+
+// Map through the Array to assign Event listeners
+buttons.map(button => {
+    button.addEventListener("click", event => {
+        num1 = event.target.textContent
+        updateResult(num1)
+    })
+})
