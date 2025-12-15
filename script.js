e3d82e5acc1d94b62d96b81e0e9e3e1dc31ec04d
Luka Stromajer <lukalel.kalulel@gmail.com>
2025-12-15 10:33:32 +0100

    Add clear button and implement getting the first number

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ function updateResult(text) {
 }
 
 
+// Clear
+let clear = Array.from(document.querySelectorAll(".clear"))
 
-
+// Map through the Array to assign Event listeners
+clear.map(item => item.addEventListener("click", event => {
+    num1 = ""
+    updateResult(num1)
+}))
 
 // First number
-let num1;
+let num1 = ""
 let buttons = Array.from(document.querySelectorAll(".num"));
 
 // Map through the Array to assign Event listeners
 buttons.map(button => {
     button.addEventListener("click", event => {
-        num1 = event.target.textContent
+        num1 += event.target.textContent
         updateResult(num1)
     })
 })
+
